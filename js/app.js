a475ca6f1b1c1165f848b4fc0b740cd609acf80b
Nolberto <nperezsullcapuma@gmail.com>
2025-06-17 14:35:43 -0500

    template

---
+++ js/app.js
@@ -0,0 +1,54 @@
+// Datos globales para interpolación
+const templateData = {
+  siteName: "Mi Sitio",
+  currentYear: new Date().getFullYear(),
+  user: "Visitante"
+};
+
+// Carga un componente y aplica variables
+function loadComponent(containerId, filePath) {
+  $.get(filePath)
+    .done(html => {
+      let processedHtml = html;
+      
+      // Reemplaza {{variables}}
+      Object.keys(templateData).forEach(key => {
+        processedHtml = processedHtml.replace(
+          new RegExp(`\\{\\{${key}\\}\\}`, 'g'), 
+          templateData[key]
+        );
+      });
+      
+      $(`#${containerId}`).html(processedHtml);
+    })
+    .fail(() => {
+      console.error(`Error cargando ${filePath}`);
+    });
+}
+
+// Carga una página dinámica
+function loadPage(pageName) {
+  $.get(`templates/${pageName}.html`)
+    .done(html => {
+      $('#content').html(html);
+      
+      // Actualiza URL sin recargar (SPA)
+      history.pushState(null, '', `#${pageName}`);
+    })
+    .fail(() => {
+      $('#content').html('<p>Error cargando la página.</p>');
+    });
+}
+
+// Navegación SPA
+$(document).on('click', '[data-page]', function(e) {
+  e.preventDefault();
+  const page = $(this).data('page');
+  loadPage(page);
+});
+
+// Manejar botón Atrás/Adelante
+$(window).on('popstate', () => {
+  const page = window.location.hash.replace('#', '') || 'home';
+  loadPage(page);
+});
